c128354592a47811137949a0121f06b1c6704635
TinMyo Win <tinmyowin@utycc.edu.mm>
2022-07-10 17:12:44 +0630

    Moving Shadow

---
+++ mouse-move-shadow/script.js
@@ -0,0 +1,26 @@
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 500;
+
+function shadow(e) {
+    const {offsetWidth: width, offsetHeight: height} = hero;
+    let {offsetY: y, offsetX: x} = e;
+
+    if(this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 1),
+        ${xWalk * -1}px ${yWalk}px 0 rgba(0, 255, 255, 1),
+        ${xWalk}px ${yWalk * -1}px 0 rgba(0, 0, 255, 1),
+        ${xWalk * -1}px ${yWalk * -1}px 0 rgba(0, 255, 0, 1)
+        ` ;
+
+}
+
+hero.addEventListener('mousemove', shadow);
